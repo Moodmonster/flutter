5458c8df25b3cf62d94ea64a8317f6e5ed7e65bf
mskwon02 <121784261+mskwon02@users.noreply.github.com>
2025-03-20 19:01:54 +0900

    design:새로운 소설 추가시 썸네일 AI생성 프롬프트입력가능

--- lib/pages/content_screen.page_view.dart
+++ lib/pages/content_screen.page_view.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:moodmonster/config/routes/routes.dart';
 import 'package:moodmonster/dumyData/contentDumyData.dart';
 import 'package:moodmonster/helpers/constants/app_colors.dart';
+import 'package:moodmonster/helpers/constants/app_typography.dart';
 import 'package:moodmonster/models/content.model.dart';
 
-class WebToonScreen extends StatelessWidget {
-  WebToonScreen({super.key});
+class ContentScreen extends StatelessWidget {
+  final MyContentType contentType;
+  ContentScreen({super.key, required this.contentType});
 
   //콘텐츠 더미데이터 가져온다
   final contentList = ContentDumyData;
@@ -13,11 +15,16 @@ class WebToonScreen extends StatelessWidget {
   //Days? selectedDay = getCurrentDay(); //우선 현재 요일로 초기화
   @override
   Widget build(BuildContext context) {
-    //맨 위에 크게 보여줄 웹툰 하나 고른다
-    final webtoon = contentList.isNotEmpty ? contentList[0] : null;
-
+    //컨텐츠 타입에 맞는 데이터만 가져온다
+    final contentTypeFilteredList =
+        contentList
+            .where((content) => content.contentType == contentType)
+            .toList();
+    //맨 위에 크게 보여줄 작품 하나 고른다
+    final Content? mainContent =
+        contentTypeFilteredList.isNotEmpty ? contentTypeFilteredList[0] : null;
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10),
       child: Column(
         children: [
           Align(
@@ -36,20 +43,22 @@ class WebToonScreen extends StatelessWidget {
             padding: const EdgeInsets.symmetric(vertical: 6),
             child: GestureDetector(
               onTap: () {
-                Navigator.pushNamed(
-                  context,
-                  Routes.episodeListScreen, // /episodes 경로로 이동
-                  arguments: webtoon, // 컨텐츠 정보 전달해준다
-                );
+                if (mainContent != null) {
+                  Navigator.pushNamed(
+                    context,
+                    Routes.episodeListScreen, // /episodes 경로로 이동
+                    arguments: mainContent, // 컨텐츠 정보 전달해준다
+                  );
+                }
               },
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(16),
                 child: Stack(
                   alignment: Alignment.bottomLeft,
                   children: [
-                    webtoon != null
+                    mainContent != null
                         ? Image.asset(
-                          webtoon.thumbnailUrl,
+                          mainContent.thumbnailUrl,
                           width: double.infinity,
                           height: 280,
                           fit: BoxFit.cover,
@@ -61,7 +70,7 @@ class WebToonScreen extends StatelessWidget {
                         ),
 
                     // 제목과 설명 추가
-                    if (webtoon != null)
+                    if (mainContent != null)
                       Container(
                         width: double.infinity,
                         padding: const EdgeInsets.all(12),
@@ -73,7 +82,7 @@ class WebToonScreen extends StatelessWidget {
                           mainAxisSize: MainAxisSize.min,
                           children: [
                             Text(
-                              webtoon.title,
+                              mainContent.title,
                               style: TextStyle(
                                 color: Colors.white,
                                 fontSize: 20,
@@ -82,7 +91,7 @@ class WebToonScreen extends StatelessWidget {
                             ),
                             SizedBox(height: 4),
                             Text(
-                              webtoon.desc,
+                              mainContent.desc,
                               maxLines: 2,
                               overflow: TextOverflow.ellipsis,
                               style: TextStyle(
@@ -103,12 +112,12 @@ class WebToonScreen extends StatelessWidget {
           Align(
             alignment: Alignment.centerRight,
             child: Text(
-              "총 ${contentList.length} 작품",
+              "총 ${contentTypeFilteredList.length} 작품",
               style: TextStyle(fontSize: 13, color: Colors.white70),
             ),
           ),
           SizedBox(height: 4),
-          ContentMainGridList(),
+          ContentMainGridList(contentList: contentTypeFilteredList),
         ],
       ),
     );
@@ -116,19 +125,35 @@ class WebToonScreen extends StatelessWidget {
 }
 
 class ContentMainGridList extends StatelessWidget {
-  //웹툰 데이터 가져온다
-  final List<Content> contentList =
-      ContentDumyData.where(
-        (data) => data.contentType == ContentType.Webtoon,
-      ).toList();
-  ContentMainGridList({super.key}) {}
+  //콘텐츠 데이터 리스트
+  final List<Content> contentList;
+  ContentMainGridList({super.key, required this.contentList}) {}
 
   @override
   Widget build(BuildContext context) {
     //return Container(color: Colors.red, height: 200);
+    //리스트에 아무 콘텐츠도 없으면
+    if (contentList.isEmpty) {
+      return Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          children: [
+            Icon(Icons.folder_off_outlined, color: AppColors.mainTextColor),
+            Text(
+              "No Data",
+              style: AppTypography.subTitle_1.copyWith(
+                color: AppColors.mainTextColor,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
     return GridView.builder(
       shrinkWrap: true,
       itemCount: contentList.length,
+      physics: NeverScrollableScrollPhysics(),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 3, // 한 줄에 3개씩 배치
         crossAxisSpacing: 8, // 가로 간격
